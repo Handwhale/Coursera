14eff509671df53b691b1fd971eedb42e84a7bfb
Nick <mr.rabbiton@gmail.com>
2019-07-07 21:15:53 +0300

    Task1

---
+++ WhiteBelt/Week1/Task1.cpp
@@ -0,0 +1,13 @@
+// https://www.coursera.org/learn/c-plus-plus-white/programming/9LFl2/a-b
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+    cout << a + b;
+    return 0;
+}
