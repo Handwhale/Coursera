24cb410987903deab0f0264d15487bcbe0e8c285
Nick <mr.rabbiton@gmail.com>
2019-10-04 18:30:44 +0300

    Group heavy strings

---
+++ RedBelt/Week5/Группировка строк/group_heavy_strings.cpp
@@ -0,0 +1,73 @@
+#include "test_runner.h"
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Объявляем Group<String> для произвольного типа String
+// синонимом vector<String>.
+// Благодаря этому в качестве возвращаемого значения
+// функции можно указать не малопонятный вектор векторов,
+// а вектор групп — vector<Group<String>>.
+template <typename String>
+using Group = vector<String>;
+
+// Ещё один шаблонный синоним типа
+// позволяет вместо громоздкого typename String::value_type
+// использовать Char<String>
+template <typename String>
+using Char = typename String::value_type;
+
+template <typename String>
+vector<Group<String>> GroupHeavyStrings(vector<String> strings)
+{
+    vector<Group<String>> sorted_vector;
+    map<String, vector<String>> chr_to_group;
+
+    for (String &str : strings)
+    {
+        String chrs = str;
+        sort(chrs.begin(), chrs.end());
+        chrs.erase(unique(chrs.begin(), chrs.end()), chrs.end());
+        chr_to_group[move(chrs)].push_back(move(str));
+    }
+
+    for (auto &item : chr_to_group)
+    {
+        sorted_vector.push_back(move(item.second));
+    }
+
+    return sorted_vector;
+}
+
+void TestGroupingABC()
+{
+    vector<string> strings = {"caab", "abc", "cccc", "bacc", "c"};
+    auto groups = GroupHeavyStrings(strings);
+    ASSERT_EQUAL(groups.size(), 2);
+    sort(begin(groups), end(groups)); // Порядок групп не имеет значения
+    ASSERT_EQUAL(groups[0], vector<string>({"caab", "abc", "bacc"}));
+    ASSERT_EQUAL(groups[1], vector<string>({"cccc", "c"}));
+}
+
+void TestGroupingReal()
+{
+    vector<string> strings = {"law", "port", "top", "laptop", "pot", "paloalto", "wall", "awl"};
+    auto groups = GroupHeavyStrings(strings);
+    ASSERT_EQUAL(groups.size(), 4);
+    sort(begin(groups), end(groups)); // Порядок групп не имеет значения
+    ASSERT_EQUAL(groups[0], vector<string>({"laptop", "paloalto"}));
+    ASSERT_EQUAL(groups[1], vector<string>({"law", "wall", "awl"}));
+    ASSERT_EQUAL(groups[2], vector<string>({"port"}));
+    ASSERT_EQUAL(groups[3], vector<string>({"top", "pot"}));
+}
+
+int main()
+{
+    TestRunner tr;
+    RUN_TEST(tr, TestGroupingABC);
+    RUN_TEST(tr, TestGroupingReal);
+    return 0;
+}
