c7e3b24d96f30b6603bb773035620ac105f8316a
Nick <mr.rabbiton@gmail.com>
2019-10-04 18:24:53 +0300

    Simple vector refactor

--- RedBelt/Week3/Пишем свой вектор/simple_vector.h
+++ RedBelt/Week3/Пишем свой вектор/simple_vector.h
@@ -1,84 +1,55 @@
 #pragma once
 
 #include <cstdlib>
+#include <algorithm>
+
+using namespace std;
 
 template <typename T>
 class SimpleVector
 {
 public:
-    SimpleVector() : _begin(nullptr), _end(nullptr), _capacity_end(nullptr) {}
-    explicit SimpleVector(size_t size)
-    {
-        _begin = new T[size];
-        _end = _begin + size;
-        _capacity_end = _capacity_end;
-    }
-    ~SimpleVector()
-    {
-        delete[] _begin;
-    }
+    SimpleVector() = default;
+    explicit SimpleVector(size_t size) : size(size), capacity(size), data(new T[size]) {}
+
+    ~SimpleVector() { delete[] data; }
 
     T &operator[](size_t index)
     {
-        return *(_begin + index);
+        return data[index];
     }
 
-    // Возможно грейдер не пустит без const вариантов
-    T *begin() { return _begin; }
-    T *end() { return _end; }
+    T *begin() { return data; }
+    T *end() { return data + size; }
 
-    size_t Size() const
-    {
-        if (_begin == nullptr)
-            return 0;
+    const T *begin() const { return data; }
+    const T *end() const { return data + size; }
 
-        return _end - _begin;
-    }
-    size_t Capacity() const
-    {
-        if (_begin == nullptr)
-            return 0;
-        return _capacity_end - _begin;
-    }
-    void PushBack(const T &value)
-    {
-        if (Size() == Capacity())
-        {
+    size_t Size() const { return size; }
+    size_t Capacity() const { return capacity; }
+
+    void PushBack(const T &value){
+        if (size == capacity){
             IncreaseCapacity();
         }
-
-        *(_end) = value;
-        _end++;
+        data[size] = value;
+        ++size;
     }
 
 private:
-    T *_begin;
-    T *_end;
-    T *_capacity_end;
+    T *data = nullptr;
+    size_t size = 0;
+    size_t capacity = 0;
 
-    void IncreaseCapacity()
-    {
-        if (Capacity() == 0)
-        {
-            // set capacity to 1
-            _begin = new T;
-            _end = _begin;
-            _capacity_end = _begin + 1;
-            return;
+    void IncreaseCapacity(){
+        if (capacity == 0){
+            capacity = 1;
+            data = new T[capacity];
         }
-        T *new_begin = new T[Capacity() * 2];
-        T *new_capacity_end = new_begin + Capacity() * 2;
-        // copy elements to new place
-        size_t old_size = Size();
-        for (int i = 0; i < old_size; i++)
-        {
-            *(new_begin + i) = *(_begin + i);
-        }
-
-        delete[] _begin;    // !!!
-
-        _begin = new_begin;
-        _end = _begin + old_size;        
-        _capacity_end = new_capacity_end;
+        capacity*= 2;
+        T* new_data = new T[capacity];
+        copy(data, &data[size], new_data);
+        delete[] data;
+        data = new_data;
     }
 };
