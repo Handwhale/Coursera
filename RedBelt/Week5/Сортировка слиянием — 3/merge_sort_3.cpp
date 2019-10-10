dd058c75bf4fe682a3cbb0b87c56f540ada46496
Nick <mr.rabbiton@gmail.com>
2019-10-11 00:36:15 +0300

    Recursive bug

---
+++ RedBelt/Week5/Сортировка слиянием — 3/merge_sort_3.cpp
@@ -0,0 +1,95 @@
+#include "test_runner.h"
+#include <algorithm>
+#include <memory>
+#include <vector>
+
+using namespace std;
+
+template <typename RandomIt>
+void MergeSort(RandomIt range_begin, RandomIt range_end)
+{
+
+  // Если диапазон содержит меньше 2 элементов, выйти из функции.
+  if (range_end - range_begin < 2)
+    return;
+
+  // Создать вектор, содержащий все элементы текущего диапазона.
+  vector<typename RandomIt::value_type> elements(make_move_iterator(range_begin), make_move_iterator(range_end));
+
+  // Разбить вектор на три равные части.
+  size_t part_vector_size = elements.size() / 3;
+
+  using ItRange = pair<move_iterator<RandomIt>, move_iterator<RandomIt>>;
+
+  ItRange range1{move_iterator(elements.begin()), move_iterator(elements.begin() + part_vector_size)};
+  ItRange range2{range1.second, move_iterator(elements.end() - part_vector_size)};
+  ItRange range3{range2.second,  move_iterator(elements.end())};
+
+  // // Вызвать функцию MergeSort от каждой части вектора.
+  MergeSort(range1.first, range1.second);
+  MergeSort(range2.first, range2.second);
+  MergeSort(range3.first, range3.second);
+
+  // // Слить первые две трети вектора с помощью алгоритма merge, сохранив результат во временный вектор с помощью back_inserter
+  vector<typename RandomIt::value_type> tmp;
+  merge(
+      range1.first, range1.second,
+      range2.first, range2.second,
+      back_inserter(tmp));
+
+  // // Слить временный вектор из предыдущего пункта с последней третью вектора из п. 2,
+  // // записав полученный отсортированный диапазон вместо исходного.
+  // merge(
+  //     move_iterator(tmp.begin()), move_iterator(tmp.end()),
+  //     move_iterator(range3.first), move_iterator(range3.second),
+  //     range_begin);
+}
+
+void TestIntVector()
+{
+  vector<int> numbers = {6, 1, 3, 9, 1, 9, 8, 12, 1};
+  MergeSort(begin(numbers), end(numbers));
+  ASSERT(is_sorted(begin(numbers), end(numbers)));
+}
+
+struct NoncopyableType
+{
+  int _data;
+
+  bool operator==(const NoncopyableType &other) const
+  {
+    return _data == other._data;
+  }
+
+  bool operator<(const NoncopyableType &other) const
+  {
+    return _data < other._data;
+  }
+
+  NoncopyableType(const NoncopyableType &) = delete;
+  NoncopyableType &operator=(const NoncopyableType &) = delete;
+
+  NoncopyableType(NoncopyableType &&) = default;
+  NoncopyableType &operator=(NoncopyableType &&) = default;
+};
+
+void TestNonCopyable()
+{
+
+  vector<NoncopyableType> data;
+  data.push_back({1});
+  data.push_back({2});
+  data.push_back({3});
+  data.push_back({4});
+  data.push_back({5});
+
+  MergeSort(begin(data), end(data));
+  ASSERT(is_sorted(begin(data), end(data)));
+}
+
+int main()
+{
+  TestRunner tr;
+  RUN_TEST(tr, TestIntVector);
+  return 0;
+}
